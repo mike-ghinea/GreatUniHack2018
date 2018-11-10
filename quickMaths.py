2a80ea3a14b54d93864f44fed8d80ad581b4f8bd
Mike <mihailemanuelghinea@gmail.com>
2018-11-10 20:17:59 +0000

    Added documentation and refactored quickMaths

--- quickMaths.py
+++ quickMaths.py
@@ -1,32 +1,65 @@
 import math
 
-def radToDeg(angle):
+def radians_to_degrees(angle):
     return angle * (180.0 / math.pi)
 
-def getHeading(x1, y1, x2, y2):
+def get_heading(thisX, thisY, thatX, thatY):
+    """
+    From player position and target position
+    return the heading that points to the target
+    (the server provides the TurnToHeading method)
+
+    Parameters
+    ----------
+    thisX : int
+        Player X coordinate
+    thisY : int 
+        Player Y coordinate
+    thatX : int
+        Target X coordinate
+    thatY : int
+        Target X coordinate
+
+    Returns
+    -------
+    int
+        Heading
+
+    """
     heading = math.atan2(y2-y1, x2-x1)
-    heading = radToDeg(heading)
+    heading = radians_to_degrees(heading)
     heading = (heading - 360) % 360
     return math.fabs(heading)
 
-def isTurnLeft(currentHeading, desiredHeading):
+def is_turn_left(currentHeading, desiredHeading):
     diff = desiredHeading - currentHeading
     if (diff > 0):
         return (diff > 180)
     else:
         return (diff >= -180)
 
-def calcDist(thisX, thisY, thatX, thatY):
+def get_distance(thisX, thisY, thatX, thatY):
+    """
+    From player position and target position
+    return the distance to the target)
+
+    Parameters
+    ----------
+    thisX : int
+        Player X coordinate
+    thisY : int 
+        Player Y coordinate
+    thatX : int
+        Target X coordinate
+    thatY : int
+        Target X coordinate
+
+    Returns
+    -------
+    float
+        Distance
+
+    """
     headingX = thatX - thisX
     headingY = thatY - thisY
-    return math.sqrt(headingX*headingX + headingY*headingY)
-
-#test values
-'''
-print "radToDeg pi = \n"
-print(radToDeg(math.pi))
-print "\ngetHeading(420, 69, 1337, 80) = \n"
-print(getHeading(420, 69, 1337, 80))
-print "\ncalcDist(420, 69, 1337, 80) = \n"
-print(calcDist(420, 69, 1337, 80))
-'''
+    return math.sqrt(headingX * headingX + headingY * cheadingY)
